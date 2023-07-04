d346caa3a7544e833527ff4232b772257ebfa69d
darkhawk <shahin.anasori@gmail.com>
2023-07-05 00:18:50 +0330

    First stable point

---
+++ ExpressFood.FoodApp.Core/Entities/ApplicationUser.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressFood.FoodApp.Core.Entities
+{
+    public class ApplicationUser
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public string Gender { get; set; }
+        public string NationalCode { get; set; }
+        public DateTime BirthDate { get; set; }
+        public string EmailAddress { get; set; }
+    }
+}
---
+++ ExpressFood.FoodApp.Infrastructure/Data/ExpressFoodFoodAppDB.cs
@@ -0,0 +1,20 @@
+﻿using ExpressFood.FoodApp.Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExpressFood.FoodApp.Infrastructure.Data
+{
+    public class ExpressFoodFoodAppDB : DbContext
+    {
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public ExpressFoodFoodAppDB(DbContextOptions<ExpressFoodFoodAppDB> options)
+            :base(options)
+        {
+            
+        }
+    }
+}
